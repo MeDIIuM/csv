2a9c76a44368177caf8636dcdce52aadd36e2cf9
Andrey <andrey@Andrey.pc>
2020-02-04 14:56:47 +0400

    migrations

---
+++ database/migrations/2020_02_04_104805_rating.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Rating extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rating', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->integer('rating');
+            $table->integer('id_user');
+            $table->integer('id_product');
+            $table->integer('id_review');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rating');
+    }
+}
